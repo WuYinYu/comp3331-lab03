5525f279c7634cf21480c77bc0848e22280dd03b
YinYuWu <z5135190@student.unsw.edu.au>
2018-08-13 14:14:33 +1000

    fix

--- WebServer.py
+++ WebServer.py
@@ -49,34 +49,3 @@ while True:
 		client_s.send(error_response_header + "\r\n")
 		client_s.send(error_content)
 		client_s.close()
-'''
-from socket import *
-
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)					# create socket for client
-serverSocket.bind(('', serverPort))							# binds address (hostname, port numer) to socket
-serverSocket.listen(1)											# Listen / wait for connections made to the socket
-print "The server is ready to receive\n"						# Arg 1 = max no. queued connections
-
-while 1:
-	print "Ready to serve"
-	connectionSocket, addr = serverSocket.accept()			# passively accepts TCP client connection
-	# Receive and send client requests
-	try:
-		request = connectionSocket.recv(1024)				# receives get request from client
-
-		message = request.split()[1];						# parse get request
-		file_name = message.replace('/', '')					# remove slash
-		print file_name, '\n'								
-
-		file = open(file_name)								# open file
-		http_output = file.read()							# read file, store as text / string
-		connectionSocket.send('HTTP/1.1 200 OK\n\n')		# send HTTP header response
-		connectionSocket.send(http_output)					# send HTTP text reponse
-		connectionSocket.close()
-	# IO Exception
-	except IOError:
-		connectionSocket.send('HTTP/1.1 404 File not found\n\n')
-		connectionSocket.send('<h1><center>404 Error: File not found</center></h1>')
-		connectionSocket.close()
-'''
